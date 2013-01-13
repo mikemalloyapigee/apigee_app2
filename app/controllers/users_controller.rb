5bec1272e744ef6f6a06f4d4e0f7d777fb4f5025
Mike Malloy <mmalloy@apigee.com>
2013-01-13 11:51:41 -0800

    Added sign up with secure passwords

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   def new
     @title = "New User"
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name unless @user.name.blank? or @user.nil?
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Apigee App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
 end
